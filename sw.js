36980c76ae7747e3d87786c1b44cd13ea119d31a
JanReimann-web <jaanes79@gmail.com>
2025-09-17 18:37:58 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Rahakaru Service Worker
+const CACHE_NAME = 'rahakaru-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/icon.png',
+  '/manifest.json'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
